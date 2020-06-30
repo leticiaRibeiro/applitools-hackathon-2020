5c476a61c247ecc6021ddec613446c7774c7a56e
Leticia Ribeiro <leticia.ribeiro@idwall.co>
2020-06-30 08:40:29 -0300

    fix: adding new assertions for product_grid

--- cypress/integration/traditional/shopping_experience.js
+++ cypress/integration/traditional/shopping_experience.js
@@ -7,5 +7,7 @@ describe('Traditional tests V1',() => {
         cy.get('input[id="colors__Black"]').check()
         cy.get('button[id="filterBtn"]').click()
         cy.get('#product_grid').children().should('have.length', 2)
+        cy.get('#product_grid').children().should('be', 'DIV__colcolmd__210')
+        cy.get('#product_grid').children().should('be', 'DIV__colcolmd__235')
     })
 })
